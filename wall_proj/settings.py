8898ae05e6f3479fbf9741c5f1cbf5344d32c79b
AishaE <aishaelahi@gmail.com>
2021-02-06 23:43:33 -0500

    first commit

--- wall_proj/settings.py
+++ wall_proj/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+from .import secrets
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f^=*pu+(o!v^rah0vjo3#m(_cz0@!mzwwi-=jq5hx8^u)$c6i%'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
